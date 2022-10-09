047f6a2e41443638d6c0a1b0fc6aacf9af3794d5
AntonKorz <akorznikov-20@edu.ranepa.ru>
2022-10-09 18:05:52 +0300

    add file

--- AAA_HW_3.py
+++ AAA_HW_3.py
@@ -1,4 +1,35 @@
-print(1)
+class CountVectorizer:
+    """класс для кодирования текста"""
+
+    def __init__(self):
+        self.feature_names = []
+        self.corpus = []
+
+    def fit_transform(self, text: list) -> list:
+        """функция принимает на вход корпус, составляет по нему словарь, который она сохраняет в атрибуты класса,
+           затем кодирует корпус по полученному словарю"""
+
+        slovarik = set()
+        for fraze in text:                    # тут мы создаем словарик, приводим к нормальному виду наш корпус
+            fraze = fraze.split()             # и сохраняем все в атрибуты класса
+            self.corpus.append(fraze)
+            slovarik = slovarik.union(fraze)
+        self.feature_names = list(slovarik)
+
+        count_matrix = [[0] * len(self.feature_names) for fraze in self.corpus]
+
+        for i, fraze in enumerate(self.corpus):
+            for j, feature in enumerate(self.feature_names):
+                for word in fraze:
+                    if word == feature:
+                        count_matrix[i][j] += 1
+
+        return count_matrix
+
+    def get_feature_names(self):
+        print(self.feature_names)
 
 
-
+a = CountVectorizer()
+print(a.fit_transform(['anton is walking', 'my name is anton korz anton', 'his name is tony', 'is this tasty']))
+a.get_feature_names()
