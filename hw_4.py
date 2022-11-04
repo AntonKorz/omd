421868bcd980b4d29601e0cf100884c22516fdc0
AntonKorz <akorznikov-20@edu.ranepa.ru>
2022-11-04 23:17:19 +0300

    исправил

--- hw_4.py
+++ hw_4.py
@@ -7,8 +7,24 @@ class ColorizeMixin:
     def __init__(self, color):
         self.repr_color_code = color
 
+    def __init_subclass__(cls, **kwargs):
+        cls.__str__ = cls.new_str
+
+    def new_str(self):
+        lipstick = self.dict2lst(self.__dict__, [])
+        strok = ''
+        for i in range(len(lipstick) - 1):
+            strok += str(lipstick[i]) + ' | '
+        else:
+            strok += str(lipstick[i + 1])
+
+        return f'\033[1;{self.repr_color_code}m' + strok
+
 
 class Json2:
+
+    def __init__(self, x):
+        pass
 
     @staticmethod
     def json2dict(json_file) -> dict:
@@ -37,14 +53,15 @@ class Json2:
 
 class Advert(ColorizeMixin, Json2):
 
-    def __init__(self, json_file, color=32, x=0):
+    def __init__(self, json_file, color=32, first_recursion_layer=True):
         super().__init__(color)
-        if x == 0:
+        if first_recursion_layer:
             self.__dict__['price'] = 0
+
         json_dict = self.json2dict(json_file)
         for key, value in json_dict.items():
             if isinstance(value, dict):
-                self.__dict__[key] = Advert(value, color, x=1)
+                self.__dict__[key] = Advert(value, color, False)
             else:
                 if keyword.iskeyword(key):
                     self.__dict__[key + '_'] = value
@@ -53,15 +70,9 @@ class Advert(ColorizeMixin, Json2):
                         raise ValueError('price < 0')
                     else:
                         self.__dict__[key] = value
-        if x == 0:
-            if 'title' not in self.__dict__.keys():
-                raise ValueError('No title')
 
-    def __setattr__(self, key, value):
-        if key == 'price' and value < 0:
-            raise ValueError('price < 0')
-        else:
-            self.__dict__[key] = value
+        if first_recursion_layer and 'title' not in self.__dict__.keys():
+            raise ValueError('No title')
 
     def __str__(self):
         lipstick = self.dict2lst(self.__dict__, [])
@@ -70,8 +81,18 @@ class Advert(ColorizeMixin, Json2):
             strok += str(lipstick[i]) + ' | '
         else:
             strok += str(lipstick[i + 1])
+        return strok
 
-        return f'\033[1;{self.repr_color_code}m' + strok
+    @property
+    def price(self, x=0):
+        print(self.price)
+
+    @price.setter
+    def price(self, value):
+        if value >= 0:
+            self.__dict__['price'] = value
+        else:
+            raise ValueError('price < 0')
 
 
 if __name__ == '__main__':
@@ -84,5 +105,6 @@ if __name__ == '__main__':
     }
     }"""
 
-    a = Advert(lesson_str, color=33)
+    a = Advert(lesson_str, color=32)
+    a.price = 10
     print(a)
